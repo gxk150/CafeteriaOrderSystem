00b17acfa15194feac2910a882b616c4c6c4f47f
gxk24233 <13286648+gxk24233@user.noreply.gitee.com>
2024-05-22 14:17:43 +0800

    initial create

--- CafeteriaOrderSystem/urls.py
+++ CafeteriaOrderSystem/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, reverse
+from django.shortcuts import HttpResponse, render
+
+from cafeteria import views
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+
+    path('cafeteria/', include('cafeteria.urls')),
+
 ]
